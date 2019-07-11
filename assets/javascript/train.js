f0303010456e8c9a6d2777b60efab4ac93de513e
Jennie K <47018372+JKGills12@users.noreply.github.com>
2019-07-11 00:58:29 -0700

    added bootstrap, firebase, moment.js, javascript src code linkes

---
+++ assets/javascript/train.js
@@ -0,0 +1,10 @@
+// Firebase config
+var firebaseConfig = {
+    apiKey: "AIzaSyD_I_QgCTn5LL3zh1GqRv_JGmI_cFQ8xWU",
+    authDomain: "jk-train-schedule.firebaseapp.com",
+    databaseURL: "https://jk-train-schedule.firebaseio.com",
+    projectId: "jk-train-schedule",
+    storageBucket: "",
+    messagingSenderId: "247418734176",
+    appId: "1:247418734176:web:ce03218b176667e1"
+};
