e3ed4085e7d048d487ac631e69acbae14393798a
Jennie K <47018372+JKGills12@users.noreply.github.com>
2019-07-11 12:28:52 -0700

    added firebase config to initialize

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,5 +1,12 @@
-// Firebase config
-var firebaseConfig = {
+// 1) Initialize Firebase
+// 2) Create button for adding new train - then update html & update database
+// 3) Create a way to retrieve 
+// 4) Create a way to determine train arrival time
+// 5) Determine the frequency of arrivals
+// 
+
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyD_I_QgCTn5LL3zh1GqRv_JGmI_cFQ8xWU",
     authDomain: "jk-train-schedule.firebaseapp.com",
     databaseURL: "https://jk-train-schedule.firebaseio.com",
@@ -8,3 +15,8 @@ var firebaseConfig = {
     messagingSenderId: "247418734176",
     appId: "1:247418734176:web:ce03218b176667e1"
 };
+
+firebase.initialize(config);
+
+var database = firebase.database();
+
