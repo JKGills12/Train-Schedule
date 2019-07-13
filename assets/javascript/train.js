19bd65d130b1668783b312a16b8d5d6a81cd7390
Jennie K <47018372+JKGills12@users.noreply.github.com>
2019-07-12 19:38:33 -0700

    set code for user input

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -16,7 +16,48 @@ var config = {
     appId: "1:247418734176:web:ce03218b176667e1"
 };
 
-firebase.initialize(config);
+firebase.initializeApp(config);
 
 var database = firebase.database();
 
+// Button for adding train
+$("#add-train-btn").on("click", function (event) {
+    event.preventDefault();
+
+// Time is 2:30 AM
+var firstTime = "2:30";
+
+
+
+// Takes user input
+
+    // Logs train name
+    var trainName = $("#train-name-input").val().trim();
+
+    // Logs train destination
+    var trainDestination = $("#destination-input").val().trim();
+
+    // Train arrives every 5 minutes
+    var trainFrequency = 5; 
+
+    // Logs train arrival time in military time
+    var militaryTime = moment(firstTime, "HH:mm");
+    console.log(militaryTime);
+
+
+    // console.log("Hello");
+
+    // Uploads new train data to the database
+    var newTrain = {
+        name: trainName,
+        destination: trainDestination,
+        frequency: trainFrequency,
+        arrival: trainArrival,
+        distance: distAway
+    };
+
+    // Upload new train data to database
+    database.ref().push(newTrain);
+   
+
+})
